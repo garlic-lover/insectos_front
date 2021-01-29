ba3ff3fd2490f9757f772ba3339614aa7d39ae59
Matthieu Poupinet <matthieupoupinet@gmail.com>
2021-01-28 22:37:22 -0600

    home page in progress

--- pages/blog/[name].js
+++ pages/blog/[name].js
@@ -1,4 +1,4 @@
-import styled from `styled-components`;
+import styled from "styled-components";
 
 import dynamic from "next/dynamic";
 
@@ -6,25 +6,28 @@ const Article = dynamic(() => import("../components/Blog/Article"), {
   ssr: false,
 });
 
-export default function ({article}){
-return <Wrapper><Article data={article}/></Wrapper>
-};
+export default function ({ article }) {
+  return (
+    <Wrapper>
+      <Article data={article} />
+    </Wrapper>
+  );
+}
 
 const Wrapper = styled.div``;
 
 export async function getStaticPaths() {
-    return {
-      paths: [{ params: { name: "first-article" } }],
-      fallback: true,
-    };
-  }
-  
-  export async function getStaticProps(params) {
-    // Use the url in the name to get the JSON from AWS
-    let name = params.params.name;
- 
-    return {
-      props: { article: "" },
-    };
-  }
-  
+  return {
+    paths: [{ params: { name: "first-article" } }],
+    fallback: true,
+  };
+}
+
+export async function getStaticProps(params) {
+  // Use the url in the name to get the JSON from AWS
+  let name = params.params.name;
+
+  return {
+    props: { article: "" },
+  };
+}
